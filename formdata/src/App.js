4c4bdde25992b3349f7b0864f01f91243e7a86de
Anjali Garg <anjaligarg@dentalkart.com>
2024-04-29 22:48:26 +0530

    made changes

--- formdata/src/App.js
+++ formdata/src/App.js
@@ -4,43 +4,48 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const[fName, setFName]= useState('');
-  const[lName, setLName] = useState('');
-  const[fullName,setFullName]= useState('');
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [fullname, setFullname] = useState("");
 
-  const handleSubmit = (e) =>{
-    e.preventDefault();
-    // if(fName && lName){
-    //   setFullName(`${fName} ${lName}`);
-    // }
+  // const handleSubmit = (e) =>{
+  //   e.preventDefault();
+  //   // if(fName && lName){
+  //   //   setFullName(`${fName} ${lName}`);
+  //   // }
 
-    if(fName === " " || fName === "" || lName === " " || lName === ""){
-      setFullName("");
-    }else{
-      setFullName(`${fName} ${lName}`);
-    }
-  } 
-  
-    
+  //   if(fName === " " || fName === "" || lName === " " || lName === ""){
+  //     setFullName("");
+  //   }else{
+  //     setFullName(`${fName} ${lName}`);
+  //   }
+  // } 
+ 
 
   return (
-    <form onSubmit={handleSubmit}>
-    
+    <div className='App'>
     <h1>Full Name Display</h1>
-    <label>First Name:
-      <input type="text" name="firstName" value={fName} onChange={(e)=>setFName(e.target.value)} required></input>
-    </label>
-    <br/>
-    <label>
-      Last Name:
-      <input type="text" name="lastName" value={lName} onChange={(e)=>setLName(e.target.value)} required></input>
-    </label>
-    <br/>
-    <button type="submit">Submit</button>
-    <br/>
-    {fullName && <p>Full Name: {fullName}</p>}
+    <form onSubmit={(e)=>{
+      e.preventDefault();
+      if(fname === " " || fname === "" || lname === " " || lname === ""){
+        setFullname("");
+      }else{
+        setFullname(`${fname} ${lname}`);
+      }
+      
+    }}>
+    <div>
+    <label>First Name:</label>
+    <input type="text" value={fname} onChange={(e)=>setFname(e.target.value)}></input>
+    </div>
+    <div>
+    <label>Last Name:</label>
+    <input type="text" value={lname} onChange={(e)=>setLname(e.target.value)}></input>
+    </div>
+    <button type="submit" >Submit</button>
     </form>
-  );
+    {fullname && <h2>Full Name: {fullname}</h2>}
+  </div>
+);
 }
-
 export default App;
